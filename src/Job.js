6de555bd5692ca30749b1a1e3cb1e164d2cb3e8f
GGG <ggg@MacBook-Pro-de-GGG.local>
2020-07-20 14:16:53 +0200

    first commit

---
+++ src/Job.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Job = () => {
+  return (
+    <div className="container">
+      <div className="item red">
+        <div className="title">Infirmiere</div>
+      </div>
+      <div className="item green">
+        <div className="title">Gardien</div>
+      </div>
+      <div className="item yellow">
+        <div className="title">Responsable d'atelier</div>
+      </div>
+      <div className="item blue">
+        <div className="title">Chef de projet</div>
+      </div>
+      <div className="item pink">
+        <div className="title">Developpeur react</div>
+      </div>
+      <div className="item red">
+        <div>title</div>
+      </div>
+      <div className="item green">
+        <div>title</div>
+      </div>
+      <div className="item yellow">
+        <div>title</div>
+      </div>
+      <div className="item blue">
+        <div>title</div>
+      </div>
+    </div>
+    //className, title, contractType, country et city
+  );
+};
+export default Job;
