1cfa8e2560e99ba61cb19f8a66f5af7cfd1b165b
GGG <ggg@MacBook-Pro-de-GGG.local>
2020-07-20 17:33:56 +0200

    ajout regles responsives

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./App.css";
 import Jobs from "./Jobs";
-import Job from "./Job";
+
 function App() {
   return (
     <div>
--- src/Job.js
+++ src/Job.js
@@ -2,9 +2,14 @@ import React from "react";
 
 const Job = () => {
   return (
+    // const Title = ({ title, contraType, country, city }) =>
     <div className="container">
+      {/* className="item" */}
       <div className="item red">
+        {/* className="title" */}
         <div className="title">Infirmiere</div>
+        {/* className=contratType */}
+        <div className="contratType"></div>
       </div>
       <div className="item green">
         <div className="title">Gardien</div>
